2f839f9b622fd6b09b3ab32e5062d51ccbe6d2a8
Craig Wann <craigwann1@gmail.com>
2016-10-06 23:43:49 -0500

    Accounting for vue already being set in the global namespace.

--- src/request.js
+++ src/request.js
@@ -1,9 +1,10 @@
-import Vue from 'vue';
 import merge from 'lodash.merge';
+import Vue from 'vue'
 
 export const request = (container, options) => {
+    if (window.Vue) Vue = window.Vue //Accounting for vue already being set in the global namespace
 
-    return Vue.$http(merge(
+    return Vue.http(merge(
         {
             headers: {
                 'Content-Type': 'application/x-www-form-urlencoded',
