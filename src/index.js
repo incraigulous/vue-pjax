dd9697fac5741f3cbf646c3c8e446895787ca480
Craig Wann <craigwann1@gmail.com>
2016-10-06 23:00:15 -0500

    PJAX container import reference was incorrect.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import handlesPjax from './mixins/handles-pjax';
 import pjax from './directives/pjax';
-import PjaxContainer from './components/pjax';
+import PjaxContainer from './components/pjax-container';
 import PjaxBus from './event-bus'
 
 export default handlesPjax;
