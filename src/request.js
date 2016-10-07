be2c74ad6d364e3a635c915f40d490d307891e01
Craig Wann <craigwann1@gmail.com>
2016-10-06 23:52:12 -0500

    The x-pjax bool causes an error in vue resource.

--- src/request.js
+++ src/request.js
@@ -10,7 +10,7 @@ export const request = (container, options) => {
                 'Content-Type': 'application/x-www-form-urlencoded',
                 'Accept': 'text/html',
                 'X-Requested-With': 'PJAX',
-                'X-PJAX': true,
+                'X-PJAX': 'true',
                 'X-PJAX-Container': `pjax-container[name="${container}"]`,
             },
         },
